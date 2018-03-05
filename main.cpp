00ac638caa4889c87bf9ebf08b955baa5227eed4
kristopher-redden <reddenkris@gmail.com>
2018-03-05 16:39:31 -0500

    Boost working

--- main.cpp
+++ main.cpp
@@ -1,56 +1,88 @@
 #include <iostream>
 #include <string>
+#include <cstddef>
 #include <fstream>
+#include <sstream>
 #include <boost/filesystem.hpp>
 
 using namespace std;
-using namespace boost::filesystem;
 
 int main(int argc, char* argv[])
 {
-    //Copy files from one location to another.
-    for (int paramCount = 1; paramCount < argc; paramCount++)
+    //http://www.cplusplus.com/reference/string/string/find_last_of/
+    //Create directory one level lower.
+    string exeLocation = argv[0];
+    size_t found = exeLocation.find_last_of("/\\");
+    string locationToWriteNewFileTo = exeLocation.substr(0,found) + "/OperatingSystemProjectOneLevelLower/";
+    boost::filesystem::create_directory(locationToWriteNewFileTo);
+
+    string fileNameToCopy;
+    string src;
+    string dst;
+    //Copy the files to the new location.
+    for (int fileNum = 1; fileNum < argc; fileNum++)
     {
-        ifstream  src(argv[paramCount], ios::binary);
-        //If the source file exists, then create a folder at the same level and copy it down.
-        if (src.good())
-        {
-            path currentPath = current_path();
-            string currentPathString = currentPath.string();
-            string fileName = currentPath.filename().string();
-            string newLocation = currentPathString + "/tempFolder";
-            create_directories(newLocation);
-            ofstream  dst(newLocation + fileName, ios::binary);
-            dst << src.rdbuf();
-        }
-        else
-        {
-            string fileNotFound = argv[paramCount];
-            cerr << fileNotFound + " file not found.";
-        }
+        src = argv[fileNum];
+        boost::filesystem::path source(src);
+
+        size_t found = src.find_last_of("/\\");
+        fileNameToCopy = src.substr(found+1);
+
+        dst = "/var/tmp/" + fileNameToCopy;//locationToWriteNewFileTo + fileNameToCopy;//"/var/tmp/" + fileNum;//
+        boost::filesystem::path destination(dst);
+        boost::filesystem::copy_file(source, destination,boost::filesystem::copy_option::overwrite_if_exists);
     }
-//    //Open the file and get the data length of the extra frame's.
-//    ifstream ifs (file1, ios::in | ios::binary);
-//    int extraFrameCharLength = fileLength % 64;
-//    if (ifs.good())
+
+
+//    if (boost::filesystem::create_directory(locationToWriteNewFileTo))
 //    {
-//        char character;
-//        //We will have 67 chars in the frame, get the data for 2 - 65.
-//        int fullFrames = fileLength / 64;
-//        int extraFrameDataLength = fileLength % 64;
-//        int allCharactersInFrame = fullFrames * 67 + extraFrameDataLength + 3;
-//        int onlyDataCharacters = fullFrames * 64 + extraFrameDataLength;
-//        unsigned char *chars = new unsigned char[onlyDataCharacters];
-//        int charCount = 0;
-//        while (ifs.get(character))
-//        {
-//            //Location 0 and 1 is SYN and ctrl, location 66 is the other SYN Char.
-//            chars[charCount] = character;
-//            charCount++;
-//        }
-//        dl.Framing(chars, file2, allCharactersInFrame, fullFrames, extraFrameDataLength, bitToFlip);
-//        ifs.close();
+//        //Directory created successfully.
+//        int x = 0;
 //    }
 //    else
-//        throw 3;
+//    {
+//        //Directory creation failed.
+//        int x = 0;
+//    }
+//    //Copy files from one location to another.
+//    for (int paramCount = 1; paramCount < argc; paramCount++)
+//    {
+//        //http://www.cplusplus.com/forum/general/116829/
+//        stringstream ss(argv[paramCount]);
+//        string token;
+//        while(getline(ss,token, '/'))
+//        {
+//            //Places what was split in the token.  What is left over is the filename.
+//        }
+//
+//        //boost::filesystem::path thePath = boost::filesystem::current_path();
+//
+//        ifstream  src(argv[paramCount], ios::binary);
+//        //If the source file exists, then create a folder at the same level and copy it down.
+//        if (src.good())
+//        {
+//
+//            string newLocation = locationToWriteNewFileTo + token;
+//            ofstream  dst(newLocation, ios::binary);
+//            dst << src.rdbuf();
+//
+//            ifstream check(newLocation, ios::binary);
+//            if (check.good())
+//            {
+//                //It was copied successfully.
+//                int x = 0;
+//            }
+//            else
+//            {
+//                //Not copied.
+//                int x = 0;
+//            }
+//        }
+//        else
+//        {
+//            string fileNotFound = argv[paramCount];
+//            cerr << fileNotFound + " file not found.";
+//            int x = 0;
+//        }
+//    }
 }
